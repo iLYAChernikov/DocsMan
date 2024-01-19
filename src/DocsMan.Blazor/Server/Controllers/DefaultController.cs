177d9cd6c932ca59cd55870a80591c4ca09e2aa6
iLYAChernikov <chernikov-i@inbox.ru>
2024-01-19 20:35:22 +0500

    Add project Blazor

---
+++ src/DocsMan.Blazor/Server/Controllers/DefaultController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocsMan.Blazor.Server.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class DefaultController : ControllerBase
+	{
+
+	}
+}
