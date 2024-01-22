1a26f4db5fc945503cd779d5da9f977db8c18edf
iLYAChernikov <chernikov-i@inbox.ru>
2024-01-22 12:36:36 +0500

    Add 'Personal Document Type' interactor

---
+++ src/DocsMan.App/Interactors/PersonalDocumentTypeExec.cs
@@ -0,0 +1,116 @@
+using DocsMan.App.Mappers;
+using DocsMan.App.Storage.RepositoryPattern;
+using DocsMan.App.Storage.Transaction;
+using DocsMan.Blazor.Shared.DTOs;
+using DocsMan.Blazor.Shared.OutputData;
+using DocsMan.Domain.Entity;
+
+namespace DocsMan.App.Interactors
+{
+	public class PersonalDocumentTypeExec
+	{
+		private IRepository<PersonalDocumentType> _repos;
+		private IUnitWork _unitWork;
+
+		public PersonalDocumentTypeExec(IRepository<PersonalDocumentType> repos, IUnitWork unitWork)
+		{
+			_repos = repos;
+			_unitWork = unitWork;
+		}
+
+		public async Task<Response<IEnumerable<PersonalDocumentTypeDto>?>> GetAll()
+		{
+			try
+			{
+				var data = ( await _repos.GetAllAsync() )?
+					.Select(x => x.ToDto());
+				return new(data);
+			}
+			catch ( Exception ex )
+			{
+				return new("Ошибка получения", ex.Message);
+			}
+		}
+
+		public async Task<Response<PersonalDocumentTypeDto?>> GetOne(int id)
+		{
+			try
+			{
+				return new(( await _repos.GetOneAsync(id) ).ToDto());
+			}
+			catch ( ArgumentNullException ex )
+			{
+				return new("Пустые входные данные", ex.ParamName);
+			}
+			catch ( NullReferenceException ex )
+			{
+				return new("Запись не найдена", ex.Message);
+			}
+			catch ( Exception ex )
+			{
+				return new("Ошибка получения", ex.Message);
+			}
+		}
+
+		public async Task<Response<PersonalDocumentTypeDto?>> GetOne(string title)
+		{
+			try
+			{
+				var ent = ( await _repos.GetAllAsync() )?
+					.FirstOrDefault(x => x.Title == title);
+				if ( ent == null )
+					return new("Запись не найдена", "Personal Doc Type not exist");
+				else
+					return new(ent.ToDto());
+			}
+			catch ( Exception ex )
+			{
+				return new("Ошибка получения", ex.Message);
+			}
+		}
+
+		public async Task<Response> Create(PersonalDocumentTypeDto dto)
+		{
+			try
+			{
+				await _repos.CreateAsync(dto.ToEntity());
+				await _unitWork.Commit();
+
+				return new(true);
+			}
+			catch ( ArgumentNullException ex )
+			{
+				return new($"Пустые входные данные: {ex.ParamName}", "Internal error of entity null props");
+			}
+			catch ( Exception ex )
+			{
+				return new("Ошибка получения", ex.Message);
+			}
+		}
+
+		public async Task<Response> Delete(int id)
+		{
+			try
+			{
+				await _repos.DeleteAsync(id);
+				await _unitWork.Commit();
+
+				return new(true);
+			}
+			catch ( ArgumentNullException ex )
+			{
+				return new("Пустые входные данные", ex.ParamName);
+			}
+			catch ( NullReferenceException ex )
+			{
+				return new("Запись не найдена", ex.Message);
+			}
+			catch ( Exception ex )
+			{
+				return new("Ошибка получения", ex.Message);
+			}
+		}
+
+
+	}
+}
